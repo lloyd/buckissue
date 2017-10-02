1bd79223a8403d623b5d13c51ef0dce99fc36021
Lloyd Hilaiel <lth@fb.com>
2017-10-02 16:40:16 -0600

    flesh out the example

--- BUCK
+++ BUCK
@@ -3,11 +3,16 @@ go_binary(
     srcs = [
         'main.go',
     ],
+    # The issue: deps inside go_binary are always assumed
+    # to be a library.  we cannot include other rules that 
+    # synthesize data files or whatnot
     deps = [
         ':generated_asset',
     ],
 )
 
+# a totally artificial example of a build rule with side affects
+# that we might want to include from a golang build target.
 genrule(
     name = 'generated_asset',
     out = 'generated_asset',
--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+       "os"
+       "fmt"
+       "io/ioutil"
+)
 
 func main() {
      fmt.Printf("Hello World!\n")
+
+     // we expect to be running via `buck run` in a generated directory
+     dir,err := os.Getwd()
+     if err != nil {
+         panic("oops")
+     }
+     fmt.Printf("We're in %s\n", dir)
+     dat, err := ioutil.ReadFile("generated_asset")
+     if err != nil {
+         fmt.Fprintf(os.Stdout, "Can't find my asset: %s\n", err)
+	 os.Exit(1)
+     }
+     fmt.Printf("My Asset: %s\n", dat)
 }
